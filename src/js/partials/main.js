92d5ccb35257500abb32b6be89e13ce40599e1b2
Artiom Mardarov <mardarov.artiom@gmail.com>
2024-11-07 17:01:47 +0200

    [Main Page] - Added necessary code for main page, adjusted styles and responsive

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,22 +1,26 @@
 const headerHiddenContainer = document.querySelector('.header-hidden');
 const body = document.querySelector('body');
+const backdrop = document.querySelector('.header-backdrop');
 const openTrigger = document.querySelector('.header-trigger.open');
 const closeTrigger = document.querySelector('.header-trigger.close');
 const navigation = document.querySelectorAll('.navigation li a');
 
-openTrigger.addEventListener('click', () => {
-  body.classList.add('navigation-open');
-  headerHiddenContainer.classList.add('open');
-});
-
-closeTrigger.addEventListener('click', () => {
+function closeNavigation() {
   headerHiddenContainer.classList.remove('open');
   headerHiddenContainer.classList.add('close');
   window.setTimeout(() => {
     headerHiddenContainer.classList.remove('close');
     body.classList.remove('navigation-open');
   }, 400)
+}
+
+openTrigger.addEventListener('click', () => {
+  body.classList.add('navigation-open');
+  headerHiddenContainer.classList.add('open');
 });
+
+backdrop.addEventListener('click', closeNavigation);
+closeTrigger.addEventListener('click', closeNavigation);
 
 window.addEventListener('load', (event) => {
   const page = window.location.pathname;
